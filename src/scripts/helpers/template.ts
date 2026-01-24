1750992ec7e906272a9bdc312fd7fce2db8aea6f
Tigran Petrosyan <tigr-080807@mail.ru>
2026-01-24 21:24:00 +0500

    Типизация template-функций

--- src/scripts/helpers/template.ts
+++ src/scripts/helpers/template.ts
@@ -1,20 +1,27 @@
 export const getTemplate = () => {
-  return document
-    .getElementById("card-template")
-    .content.querySelector(".card")
-    .cloneNode(true);
+  const template = document.getElementById(
+    "card-template",
+  ) as HTMLTemplateElement;
+  const cardElement = template.content.querySelector(".card") as HTMLLIElement;
+  return cardElement.cloneNode(true) as HTMLLIElement;
 };
 
 export const getTemplateUserCardInfo = () => {
-  return document
-    .getElementById("popup-info-user-preview-template")
-    .content.querySelector(".popup__list-item")
-    .cloneNode();
+  const template = document.getElementById(
+    "popup-info-user-preview-template",
+  ) as HTMLTemplateElement;
+  const userCardInfoElement = template.content.querySelector(
+    ".popup__list-item",
+  ) as HTMLLIElement;
+  return userCardInfoElement.cloneNode() as HTMLLIElement;
 };
 
 export const getTemplateInfoString = () => {
-  return document
-    .getElementById("popup-info-definition-template")
-    .content.querySelector(".popup__info-item")
-    .cloneNode(true);
+  const template = document.getElementById(
+    "popup-info-definition-template",
+  ) as HTMLTemplateElement;
+  const infoStringElement = template.content.querySelector(
+    ".popup__info-item",
+  ) as HTMLDivElement;
+  return infoStringElement.cloneNode(true) as HTMLDivElement;
 };
